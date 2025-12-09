892860d59ba86b2aba7cab12762d60f6b2ebe50e
ipz242_nom <ipz242_nom@student.ztu.edu.ua>
2025-12-09 23:15:22 +0200

    Refactor: розділено функцію analyze_clients на інші функція задля дотримання принципу єдиної відповідальності

--- task3.py
+++ task3.py
@@ -1,3 +1,29 @@
+def is_valid_client_format(client):
+    if (not isinstance(client, tuple) or len(client) != 3 or
+        not all(isinstance(item, str) for item in client)):
+        return False
+
+    name, status, email = client
+    if name == "" or status == "" or email == "":
+        return False
+
+    return True
+
+def count_client_statuses(clients_data, status_count):
+    status = clients_data[1]
+    if status:
+        status_count[status] = status_count.get(status, 0) + 1
+
+def collect_invalid_emails(clients_data, invalid_emails):
+    email = clients_data[2]
+    if "@" not in email or not isinstance(email, str):
+        invalid_emails.append(email)
+
+def identify_new_clients(clients_data, new_clients):
+    name, status, email = clients_data
+    if status == "новий" and name and "@" in email:
+        new_clients.append(name)
+
 def analyze_clients(clients):
     status_count = {}
     invalid_emails = []
@@ -5,24 +31,13 @@ def analyze_clients(clients):
     errors = []
 
     for client in clients:
-        if (not isinstance(client, tuple) or len(client) != 3 or
-            not all(isinstance(item, str) for item in client)):
+        if not is_valid_client_format(client):
             errors.append(client)
             continue
 
-        name, status, email = client
-
-        if name == "" or status == "" or email == "":
-            errors.append(client)
-
-        if status:
-            status_count[status] = status_count.get(status, 0) + 1
-
-        if "@" not in email:
-            invalid_emails.append(email)
-
-        if status == "новий" and name and "@" in email:
-            new_clients.append(name)
+        count_client_statuses(client, status_count)
+        collect_invalid_emails(client, invalid_emails)
+        identify_new_clients(client, new_clients)
 
     return {
         "status_count": status_count,
